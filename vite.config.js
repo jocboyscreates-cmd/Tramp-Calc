079fab736ba523b4d63ad63e6d207f2bfddf7e88
Jackson Cann <jocboyscreates@gmail.com>
2026-05-14 12:15:34 -0700

    Added saved routine editing

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
 
   plugins: [
 
     react(),
-
-    sitemap({
-
-      hostname:
-        "https://tramp-calc.vercel.app",
-
-        filename: "sitemap_index.xml",
-
-      dynamicRoutes: [
-  "/trampoline",
-  "/double-mini",
-  "/tumbling",
-  "/games",
-  "/random-routine-game",
-  "/connections-game",
-],
-
-      exclude: [
-        "/google36853433b248140f",
-      ],
-
-    }),
 
   ],
 
